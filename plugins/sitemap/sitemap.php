0ec84b3a4c4a7480fd4bf2330c9e43bd472faa4d
Awilum <awilum@msn.com>
2014-01-17 22:59:58 +0200

    Sitemap Plugin - Updates

--- plugins/sitemap/sitemap.php
+++ plugins/sitemap/sitemap.php
@@ -2,7 +2,23 @@
 
 if (Morfy::factory()->getUrl() == 'sitemap.xml') {
     Morfy::factory()->addAction('before_render', function() {
-        echo 'Sitemap';
+
+echo '
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+$pages = Morfy::factory()->getPages(CONTENT_PATH, 'date', 'DESC', array('404'));
+foreach($pages as $page) {
+echo ('<url>
+		<loc>'.$page['url'].'</loc>
+		<lastmod>'.$page['date'].'</lastmod>
+		<changefreq>weekly</changefreq>
+		<priority>1.0</priority>
+	</url>');
+}
+echo '
+</urlset>
+';
+
         exit();
     });
 }
