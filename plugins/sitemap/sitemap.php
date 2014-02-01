3a293db5ef805986e6770c443a9f4abb98556603
Sergey Romanenko <awilum@msn.com>
2014-02-01 21:25:15 +0200

    Update sitemap.php

--- plugins/sitemap/sitemap.php
+++ plugins/sitemap/sitemap.php
@@ -11,11 +11,10 @@
  *
  */
 
-header("Content-Type: text/xml; charset=utf-8");
  
 if (Morfy::factory()->getUrl() == 'sitemap.xml') {
     Morfy::factory()->addAction('before_render', function() {
-
+header("Content-Type: text/xml; charset=utf-8");
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 $pages = Morfy::factory()->getPages(CONTENT_PATH, 'date', 'DESC', array('404'));
