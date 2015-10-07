a18dc488aafc5c87bc07ab2c0e3aacaf8f1f3503
Awilum <awilum@msn.com>
2015-10-07 20:23:34 +0300

    Remove Sitemap plugin #35

--- config/Morfy.php
+++ config/Morfy.php
@@ -10,7 +10,7 @@
         'site_keywords' => 'site, keywords',
         'email' => 'admin@admin.com',
         'plugins' => array(
-            'parsedown',
+            'parsedown'
         ),
            /**
        	 * https://github.com/fenom-template/fenom/blob/master/docs/ru/configuration.md
