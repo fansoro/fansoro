6d0fb54e8995faa4de742505374b0b204563ca4d
Awilum <awilum@msn.com>
2015-10-29 00:22:22 +0300

    Morfy Config Refactoring #56

--- libraries/Morfy/Config.php
+++ libraries/Morfy/Config.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 class Config
 {
     /**
@@ -18,6 +18,16 @@ class Config
      */
     public static $config = array();
 
+    /**
+     * Set config file
+     *
+     *  <code>
+     *      Config::setFile('path_to_config_file');
+     *  </code>
+     *
+     * @access  public
+     * @param  string $path Path to config file
+     */
     public static function setFile($path)
     {
         if (File::exists($path)) {
@@ -25,11 +35,32 @@ class Config
         }
     }
 
+    /**
+     * Set new config variable
+     *
+     *  <code>
+     *      Config::set('key', 'value');
+     *  </code>
+     *
+     * @access  public
+     * @param  string $key   Key
+     * @param  mixed  $value Value
+     */
     public static function set($key, $value)
     {
         return Arr::set(static::$config, $key, $value);
     }
 
+    /**
+     * Get config variable
+     *
+     *  <code>
+     *      Config::set('key');
+     *  </code>
+     *
+     * @access  public
+     * @param  string $key   Key
+     */
     public static function get($key)
     {
         return Arr::get(static::$config, $key);
