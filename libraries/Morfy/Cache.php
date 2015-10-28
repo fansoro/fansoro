86cf314e46a98346549c6b6d4717230a7748189f
Awilum <awilum@msn.com>
2015-10-29 01:39:12 +0300

    Add Cache #52

---
+++ libraries/Morfy/Cache.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the Morfy.
+ *
+ * (c) Romanenko Sergey / Awilum <awilum@msn.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Cache
+{
+}
