e6d1ac06171bc92e736f06ffe9ad6b2747a7f9d8
Awilum <awilum@msn.com>
2015-10-29 23:03:14 +0300

    Add Cache #52

--- libraries/Morfy/Cache.php
+++ libraries/Morfy/Cache.php
@@ -11,4 +11,51 @@
 
 class Cache
 {
+    /**
+     * @var DoctrineCache
+     */
+    protected $driver;
+
+    /**
+     * An instance of the Cache class
+     *
+     * @var object
+     */
+    protected static $instance = null;
+
+    /**
+     * Protected clone method to enforce singleton behavior.
+     *
+     * @access  protected
+     */
+    protected function __clone()
+    {
+        // Nothing here.
+    }
+
+    /**
+     * Constructor.
+     *
+     * @access  protected
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Initialize Morfy Plugins
+     *
+     *  <code>
+     *      Cache::init();
+     *  </code>
+     *
+     * @access  public
+     */
+    public static function init()
+    {
+        if (! isset(self::$instance)) {
+            self::$instance = new Cache();
+        }
+        return self::$instance;
+    }
 }
