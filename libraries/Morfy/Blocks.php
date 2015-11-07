06891e42c211fd2cb39e3b5d1e1ac9bbb91354a9
Awilum <awilum@msn.com>
2015-11-08 02:02:38 +0300

    Add Cache #52

--- libraries/Morfy/Blocks.php
+++ libraries/Morfy/Blocks.php
@@ -24,16 +24,19 @@ class Blocks
      */
     public static function get($name)
     {
-        if (Cache::driver()->contains('block.'.$name)) {
-            return Cache::driver()->fetch('block.'.$name);
-        } else {
-            if (File::exists($block_path = BLOCKS_PATH . '/' . $name . '.md')) {
-                $block = Markdown::parse(file_get_contents($block_path));
-                Cache::driver()->save('block.'.$name, $block);
-                return $block;
+        if (File::exists($block_path = BLOCKS_PATH . '/' . $name . '.md')) {
+
+            // Create Unique Cache ID for Block
+            $block_cache_id = md5('block' . ROOT_DIR . filemtime(BLOCKS_PATH . '/' . $name . '.md'));
+
+            if (Cache::driver()->contains($block_cache_id)) {
+                return Cache::driver()->fetch($block_cache_id);
             } else {
-                return 'Block '.$name.' is not found!';
+                Cache::driver()->save($block_cache_id, $block = Markdown::parse(file_get_contents($block_path)));
+                return $block;
             }
+        } else {
+            return 'Block '.$name.' is not found!';
         }
     }
 }
